bb6d98980dccdd84321ca6011c41b7d11d934c29
Julius Cebreros <me@juliuscebreros.com>
2017-03-01 22:52:26 +1300

    Test ensureloggedin component

--- app/containers/EnsureLoggedIn/tests/index.test.js
+++ app/containers/EnsureLoggedIn/tests/index.test.js
@@ -1,10 +1,28 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import { EnsureLoggedIn } from '../index';
+import { EnsureLoggedIn } from '../index';
 
 describe('<EnsureLoggedIn />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('Show children when logged in', () => {
+    const isLoggedIn = true;
+    const renderedComponent = shallow(
+      <EnsureLoggedIn isLoggedIn={isLoggedIn}>
+        <span>I am a child</span>
+      </EnsureLoggedIn>
+    );
+
+    expect(renderedComponent.contains((<span>I am a child</span>))).toEqual(true);
+  });
+
+  it('Should show null when not logged in', () => {
+    const isLoggedIn = false;
+    const renderedComponent = shallow(
+      <EnsureLoggedIn isLoggedIn={isLoggedIn}>
+        <span>I am a child</span>
+      </EnsureLoggedIn>
+    );
+
+    expect(renderedComponent.get(0)).toEqual(null);
   });
 });
