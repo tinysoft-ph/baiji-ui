14ae0868a5b40dd4802feb8332cd1d9b128504b7
Julius Cebreros <me@juliuscebreros.com>
2017-03-02 23:34:56 +1300

    Add global reducer, add test for token get

--- app/containers/App/actions.js
+++ app/containers/App/actions.js
@@ -1,6 +1,7 @@
 
 import {
   NAVIGATE_TO,
+  ME_FROM_TOKEN,
 } from './constants';
 
 
@@ -8,5 +9,12 @@ export function navigateTo(url) {
   return {
     type: NAVIGATE_TO,
     url,
+  };
+}
+
+export function meFromToken(token) {
+  return {
+    type: ME_FROM_TOKEN,
+    token,
   };
 }
