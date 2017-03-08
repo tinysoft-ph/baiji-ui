a308de3152a14385327813ce5f6ca720ce094f0f
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2017-03-08 21:49:11 +1300

    WIP Add saga

--- app/containers/LoginPage/sagas.js
+++ app/containers/LoginPage/sagas.js
@@ -1,11 +1,20 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { take, call, put, select } from 'redux-saga/effects';
+
+import { changeUsername, changeUsernameError } from './actions';
 
 // Individual exports for testing
-export function* defaultSaga() {
-  // See example in containers/HomePage/sagas.js
+export function* getUsername() {
+  const url = 'http://localhost:3000/username';
+
+  try {
+    const username = yield call(url, 'test');
+    yield put(changeUsername(username));
+  } catch (err) {
+    yield put(changeUsernameError(err));
+  }
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  getUsername,
 ];
