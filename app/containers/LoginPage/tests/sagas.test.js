de4a1ecda7e38c6c4ec4ca24f1c4ab11eb9e0b6a
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2017-03-14 22:44:06 +1300

    Update saga tests

--- app/containers/LoginPage/tests/sagas.test.js
+++ app/containers/LoginPage/tests/sagas.test.js
@@ -4,34 +4,33 @@
 
 /* eslint-disable redux-saga/yield-effects */
 import { take, call, put, select } from 'redux-saga/effects';
-import { loginSuccess } from '../actions';
-import { getUsername, submitLoginForm } from '../sagas';
-
-// const generator = defaultSaga();
-
-// describe('getUsername Saga', () => {
-//   it('should dispatch the changeUsername action if it requests the data successfully', () => {
-//     const response = {
-//       username: 'test',
-//     };
-//     const putDescriptor = getReposGenerator.next(reponse).value;
-//
-//     expect(putDescriptor).toEqual(put(usernameLoaded(username)));
-//   });
-// });
+import { loginSuccess, loginFailed } from '../actions';
+import { submitLoginForm } from '../sagas';
 
 describe('submitLoginForm Saga', () => {
+  let submitLoginFormGenerator;
+
+  beforeEach(() => {
+    submitLoginFormGenerator = submitLoginForm();
+    const testCall = submitLoginFormGenerator.next().value;
+
+    expect(testCall).toMatchSnapshot();
+  });
+
   it('should dispatch the loginSuccess action it request returns successfully', () => {
-    const callDescriptor = submitLoginForm().next().value;
     const fixture = {
       firstName: 'firstName',
       lastName: 'lastName',
     };
+    const callDescriptor = submitLoginFormGenerator.next(fixture).value;
 
     expect(callDescriptor).toEqual(put(loginSuccess(fixture)));
   });
 
   it('should dispatch the loginFailed action it request returns unsuccessfully', () => {
-    expect(true).toEqual(false);
+    const fixture = 'Error';
+    const callDescriptor = submitLoginFormGenerator.throw(fixture).value;
+
+    expect(callDescriptor).toEqual(put(loginFailed(fixture)));
   });
 });
