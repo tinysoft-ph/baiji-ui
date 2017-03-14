87374dd5e6e48b3a4a96bac9e845664ce0c330c7
Julius Cebreros <me@juliuscebreros.com>
2017-03-14 23:00:09 +1300

    Add saga for generating me data

--- app/containers/App/sagas.js
+++ app/containers/App/sagas.js
@@ -1,11 +1,23 @@
-import { takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 
 import {
   ME_FROM_TOKEN,
 } from './constants';
 
-function* getMeFromToken() {
+import {
+  meRequestFailed,
+  meRequestSuccess,
+} from './actions';
 
+function* getMeFromToken(action) {
+  // TODO: placeholder for request
+  const request = {};
+  try {
+    const me = yield call(request.fetch, action.username);
+    yield put(meRequestSuccess(me));
+  } catch (err) {
+    yield put(meRequestFailed(err.message));
+  }
 }
 
 function* meData() {
