208ca7e7430f8f83bc5e3c0289ecdd64d4f17f41
Julius Cebreros <me@juliuscebreros.com>
2017-03-24 19:52:04 +1300

    Profile loaded reducers

--- app/containers/ProfilePage/reducer.js
+++ app/containers/ProfilePage/reducer.js
@@ -7,6 +7,8 @@
 import { fromJS } from 'immutable';
 import {
   LOAD_PROFILE,
+  PROFILE_LOADED,
+  PROFILE_LOADED_FAILED,
 } from './constants';
 
 const initialState = fromJS({
@@ -17,6 +19,10 @@ function profilePageReducer(state = initialState, action) {
   switch (action.type) {
     case LOAD_PROFILE:
       return state.set('isLoading', true);
+    case PROFILE_LOADED:
+      return state.set('isLoading', false);
+    case PROFILE_LOADED_FAILED:
+      return state.set('isLoading', false);
     default:
       return state;
   }
