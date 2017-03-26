d95712de8b651bde16a6e9d2c3eb756d5c0090d5
Julius Cebreros <me@juliuscebreros.com>
2017-03-26 21:47:51 +1300

    Add profile page results action

--- app/containers/ProfilePage/actions.js
+++ app/containers/ProfilePage/actions.js
@@ -6,10 +6,26 @@
 
 import {
   LOAD_PROFILE,
+  PROFILE_LOADED,
+  PROFILE_LOAD_FAILED,
 } from './constants';
 
 export function loadProfile() {
   return {
     type: LOAD_PROFILE,
+  };
+}
+
+export function loadProfileSuccess(user) {
+  return {
+    type: PROFILE_LOADED,
+    user,
+  };
+}
+
+export function loadProfileFailed(message) {
+  return {
+    type: PROFILE_LOAD_FAILED,
+    message,
   };
 }
--- app/containers/ProfilePage/tests/actions.test.js
+++ app/containers/ProfilePage/tests/actions.test.js
@@ -1,9 +1,13 @@
 
 import {
   loadProfile,
+  loadProfileSuccess,
+  loadProfileFailed,
 } from '../actions';
 import {
   LOAD_PROFILE,
+  PROFILE_LOADED,
+  PROFILE_LOAD_FAILED,
 } from '../constants';
 
 describe('ProfilePage actions', () => {
@@ -12,7 +16,33 @@ describe('ProfilePage actions', () => {
       const expected = {
         type: LOAD_PROFILE,
       };
-      expect(loadProfile()()).toEqual(expected);
+      expect(loadProfile()).toEqual(expected);
+    });
+  });
+
+  describe('PROFILE_LOADED Action', () => {
+    it('has a type of PROFILE_LOADED', () => {
+      const fixture = {
+        name: 'username',
+      };
+      const expected = {
+        type: PROFILE_LOADED,
+        user: fixture,
+      };
+
+      expect(loadProfileSuccess(fixture)).toEqual(expected);
+    });
+  });
+
+  describe('PROFILE_LOAD_FAILED Action', () => {
+    it('has a type of PROFILE_LOAD_FAILED', () => {
+      const fixture = 'message';
+      const expected = {
+        type: PROFILE_LOAD_FAILED,
+        message: fixture,
+      };
+
+      expect(loadProfileFailed(fixture)).toEqual(expected);
     });
   });
 });
