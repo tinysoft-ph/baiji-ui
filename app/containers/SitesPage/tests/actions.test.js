e2853a08fa3c6924a26a1af06bd136b985610d77
Julius Cebreros <me@juliuscebreros.com>
2017-04-01 23:21:38 +1300

    Add actions and constants to Sites page

--- app/containers/SitesPage/tests/actions.test.js
+++ app/containers/SitesPage/tests/actions.test.js
@@ -1,18 +1,49 @@
 
 import {
-  defaultAction,
+  loadSites,
+  loadSitesSuccess,
+  loadSitesFailed,
 } from '../actions';
+
 import {
-  DEFAULT_ACTION,
+  LOAD_SITES,
+  LOAD_SITES_SUCCESS,
+  LOAD_SITES_FAILED,
 } from '../constants';
 
 describe('SitesPage actions', () => {
-  describe('Default Action', () => {
-    it('has a type of DEFAULT_ACTION', () => {
+  describe('Load Site Action', () => {
+    it('has a type of LOAD_SITES', () => {
       const expected = {
-        type: DEFAULT_ACTION,
+        type: LOAD_SITES,
       };
-      expect(defaultAction()).toEqual(expected);
+      expect(loadSites()).toEqual(expected);
+    });
+  });
+
+  describe('Load Site Success', () => {
+    it('has a type of LOAD_SITES_SUCCESS', () => {
+      const fixture = [
+        { name: 'Site1' },
+        { name: 'Site2' },
+      ];
+      const expected = {
+        type: LOAD_SITES_SUCCESS,
+        sites: fixture,
+      };
+
+      expect(loadSitesSuccess(fixture)).toEqual(expected);
+    });
+  });
+
+  describe('Load Site Failed', () => {
+    it('has a type of LOAD_SITES_FAILED', () => {
+      const fixture = 'Error Message';
+      const expected = {
+        type: LOAD_SITES_FAILED,
+        message: fixture,
+      };
+      expect(loadSitesFailed(fixture)).toEqual(expected);
     });
   });
 });
