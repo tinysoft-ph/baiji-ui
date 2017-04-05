51aabfb5ea3c99cc7ba9462c175db7b55805d3ac
Julius Cebreros <me@juliuscebreros.com>
2017-04-05 15:32:57 +1200

    Follow naming convention in action

--- app/containers/ProfilePage/constants.js
+++ app/containers/ProfilePage/constants.js
@@ -5,7 +5,7 @@
  */
 
 export const LOAD_PROFILE = 'baiji/ProfilePage/LOAD_PROFILE';
-export const PROFILE_LOAD_SUCCESS = 'baiji/ProfilePage/PROFILE_LOADED';
+export const PROFILE_LOAD_SUCCESS = 'baiji/ProfilePage/PROFILE_LOADED_SUCCESS';
 export const PROFILE_LOAD_FAILED = 'baiji/ProfilePage/PROFILE_LOAD_FAILED';
 export const EDIT_PROFILE = 'baiji/ProfilePage/EDIT_PROFILE';
 export const EDIT_PROFILE_SUCCESS = 'baiji/ProfilePage/EDIT_PROFILE_SUCCESS';
