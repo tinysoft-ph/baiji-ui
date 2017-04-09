439e82b16b740bcac917beb024ce30605e649076
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2017-04-09 23:26:58 +1200

    WIP Add saga

--- app/containers/SitesView/sagas.js
+++ app/containers/SitesView/sagas.js
@@ -1,11 +1,28 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { takeLatest, call, put } from 'redux-saga/effects';
+
+import request from 'utils/request';
+
+import {
+  LOAD_SITE,
+} from './constants';
+
+export function* loadSiteView(id) {
+  const url = `/site?id=${id}`;
+
+  try {
+    const view = yield call(request, url);
+    yield put(view);
+  } catch (err) {
+    yield put(err.message);
+  }
+}
 
 // Individual exports for testing
-export function* defaultSaga() {
-  // See example in containers/HomePage/sagas.js
+export function* getSiteView() {
+  yield takeLatest(LOAD_SITE, loadSiteView());
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  getSiteView,
 ];
